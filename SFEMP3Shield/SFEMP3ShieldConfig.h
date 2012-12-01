fcadbb011b0da2e1776d558b56e09222b6457fd6
Michael P. Flaga <michael@flaga.net>
2012-12-01 00:39:27 -0500

    Initial work adding MarkDown Support for creating reference documentation using Doxygen

--- SFEMP3Shield/SFEMP3ShieldConfig.h
+++ SFEMP3Shield/SFEMP3ShieldConfig.h
@@ -1,59 +1,127 @@
-/******************************************************************
+/**
+\file SFEMP3ShieldConfig.h
+
+\brief Hardware dependent configuration definitions
+\remarks implemented with Doxygen Markdown format
+
 This SFEMP3ShieldConfig.h helps configure the SFEMP3Shield library for 
 various supported different base Arduino boards and shield's using the
-VS1053 chip. It is possible this may support other VS10xx chips. But are
+VS10xx chip. It is possible this may support other VS10xx chips. But are
 unverified at this time.
 
-HARDWARE SUPPORT :
 As the name SFEMP3Shield implies this driver was originally developed for 
 Sparkfun's MP3 Player Shield. Whereas it can and has been easily adapted 
 to other hardware, both base Arduino's and shield's using the VS10xx.
 
 The default configuration of this library assumes the SFE MP3 Shield, on 
 an UNO/Duemilanove, when left un-altered.
-******************************************************************/
+ 
+\note MEGA:
+Arduino Mega's REQUIRES additional jumpers. As the SPI are not on the same
+pins as the UNO/Duemilanove.
+When using a mega with SFE or Seeduino jump the following pings : 
+<tt>
+\n Mega's 51 to the MP3's D11 for MOSI
+\n Mega's 50 to the MP3's D12 for MISO
+\n Mega's 52 to the MP3's D13 for SCK 
+</tt>
+\n The remainder of pins may remain unchanged. Including INT0 as the Mega maps
+INT0 to D2 as to support USE_MP3_INTx, as is. Like the Uno.
+\n Yes, SdFat's SoftSPI.h was tried, but has problems when used twice once with
+Sd2Card.cpp and a 2nd time with SFEMP3Shield.cpp.
+
+\sa see also www.sparkfun.com
+ */
+
 #ifndef SFEMP3ShieldConfig_h
 #define SFEMP3ShieldConfig_h
 
 //------------------------------------------------------------------------------
+
 /**
- * MEGA:
- * Arduino Mega's REQUIRES additional jumpers. As the SPI are not on the same
- * pins as the UNO/Duemilanove.
- * When using a mega with SFE or Seeduino jump the following pings : 
- *   Mega's 51 to the MP3's D11 for MOSI,
- *   Mega's 50 to the MP3's D12 for MISO
- *   Mega's 52 to the MP3's D13 for SCK 
- * The remainder of pins may remain unchanged. Including INT0 as the Mega maps
- * INT0 to D2 as to support USE_MP3_INTx, as is. Like the Uno.
+ * \def SEEEDUINO
+ * \brief A macro to configure use on a Seeeduino MP3 player shield
  *
- * Note: SdFat's SoftSPI.h was tried, but has problems when used twice once with
- * Sd2Card.cpp and a 2nd time with SFEMP3Shield.cpp.
- */
-
-//------------------------------------------------------------------------------
-/**
- * SEEEDUINO:
  * Seeduino MP3 Players is supported. However, its DREQ is not connected to a 
- * hard INT(x) pin, hence it MUST be polled. This can be configured below, using
+ * hard INT(x) pin, hence it MUST be polled. This can be configured, using
  * USE_MP3_SimpleTimer.
- * When using a Seeeduino MP3 Player shield uncomment the below define of SEEEDUINO
+ * When using a Seeeduino MP3 Player shield set the below define of SEEEDUINO to 1
  * Along with USE_MP3_REFILL_MEANS should not be USE_MP3_INTx, unless extra wires.
+ *
+ * Set \c SEEEDUINO to \c 0 to use on a SparkFun MP3 player shield
+ *
+ * Set \c SEEEDUINO to \c 1 to use on a Seeeduino MP3 player shield
+ *
  */
-//#defined SEEEDUINO  // uncomment if using the Seeeduino Music Shield
+#define SEEEDUINO 0 // uncomment if using the Seeeduino Music Shield
 
 //------------------------------------------------------------------------------
-/**
+/*
  * MP3 Player Shield pin mapping. See the appropiate schematic
  */
-#if defined( SEEEDUINO )
+
+/**
+ * \def MP3_XCS
+ * \brief A macro to configure the XCS pin
+ *
+ * VS10xx's Control Chip Select Pin (for accessing SPI Control/Status registers)
+ * as seen by the the Arduino
+ *
+ */
+
+/**
+ * \def MP3_XDCS
+ * \brief A macro to configure the XDCS pin
+ *
+ * VS10xx's Data Chip Select Pin (for streaming data back and forth)
+ * as seen by the the Arduino 
+ */
+
+/**
+ * \def MP3_DREQ
+ * \brief A macro to configure the DREQ pin
+ *
+ * VS10xx's DREQ pin that indicates when it is clear to send more data.
+ * aka Data REQuest.
+ * as seen by the the Arduino 
+ */
+
+/**
+ * \def MP3_DREQINT
+ * \brief A macro to configure the DREQINT pin
+ *
+ * The associated INT(X) pin name for the associated pin of DREQ, if used.
+ * as seen by the the Arduino
+ * 
+ * This may not be needed when USE_MP3_REFILL_MEANS is not equal to USE_MP3_REFILL_MEANS
+ *
+ * \sa USE_MP3_REFILL_MEANS
+ */
+
+/**
+ * \def MP3_RESET
+ * \brief A macro to configure the RESET pin
+ *
+ * VS10xx's RESET Pin
+ * as seen by the the Arduino */
+
+/**
+ * \def SD_SEL
+ * \brief A macro to configure the SdCard Chip Select for SFEMP3SHield library
+ * 
+ * This is the pin of the Arduino that is connected to the SdCards Chip select pin.
+ * This pin should be the same pin assigned in SdFat Library.
+ * as seen by the the Arduino */
+
+// if SEEDUINO use the following pin outs
+#if ( SEEEDUINO == 1 )
 #define MP3_XCS             A3 //Control Chip Select Pin (for accessing SPI Control/Status registers)
 #define MP3_XDCS            A2 //Data Chip Select / BSYNC Pin
 #define MP3_DREQ            A1 //Data Request Pin: Player asks for more data
 //#define MP3_DREQINT        0 // There is no IRQ used on Seeduino
 #define MP3_RESET           A0 //Reset is active low
 #define SD_SEL              10 //select pin for SD card
-#else // otherwise typical Arduino Uno/Duemilanove
+#else // otherwise use pinout of typical Sparkfun MP3 Player Shield.
 #define MP3_XCS              6 //Control Chip Select Pin (for accessing SPI Control/Status registers)
 #define MP3_XDCS             7 //Data Chip Select / BSYNC Pin
 #define MP3_DREQ             2 //Data Request Pin: Player asks for more data
@@ -64,46 +132,91 @@ an UNO/Duemilanove, when left un-altered.
 
 //------------------------------------------------------------------------------
 /**
- * The selection of DREQ and its refilling method.
+ * \def USE_MP3_REFILL_MEANS
+ * \brief The selection of DREQ'ss refilling method.
+ *
+ * The value is that of an enumerated list of possible methods, aka means.
+ * 
+ * The VS10xx's DREQ requests more data from the host micro (the Arduino) 
+ * Where it can either be an input to an interrupt and corresponding ISR or 
+ * be polled by software. 
+ *
  * To enable MP3 Player to use OTHER than default INT0 on D2 for refilling 
- * uncomment the below define of USE_MP3_REFILL_MEANS and set to desired method, 
- * described below. 
+ * uncomment or change the define of USE_MP3_REFILL_MEANS and set to desired method, 
+ * 
  * The default when left commented implements INT0 on D2.
+ *
+ * \note ALL base Arduino's should support timers and soft polled means of either
+ * \n USE_MP3_Polled, USE_MP3_Timer1 or USE_MP3_SimpleTimer means. 
+ * \n Assuming resources are not committed else where.
+ * 
+ * \warning Remember to restart Arduino IDE for new Libraries to be available. 
+ * Coping the file is not enough.
  */
-//#define USE_MP3_REFILL_MEANS USE_MP3_INTx
+#define USE_MP3_REFILL_MEANS USE_MP3_INTx
 
-//------------------------------------------------------------------------------
-/**
+/*
  * Configure the implemented means of Refilling the VS10xx chip
  */
 #if defined(USE_MP3_REFILL_MEANS)
-#define USE_MP3_INTx        0 // defacto Interrupt on INTx, from DREQ
-#define USE_MP3_Polled      1 // simply polled from main loop checks DREQ on period.
-#define USE_MP3_Timer1      2 // Interrupt on Timer1, timer IRQ checks DREQ on period.
-#define USE_MP3_SimpleTimer 3 // Soft SimpleTimer period, main loop checks DREQ on period. Note this uses 170 more bytes.
-#endif
+
 /**
- * Note ALL base Arduino's should support timers and soft polled means of either
- * USE_MP3_Polled, USE_MP3_Timer1 or USE_MP3_SimpleTimer means. 
- * Assuming resources are not committed else where.
+ *\brief defacto Interrupt on INTx, from DREQ
+ * \brief A macro of the enumerated value used to select hard interrupt INTx as the means to refill the VS10xx
  *
- * Note INT(x) may not be available on some base systems depending upon design.
+ * Where the Interrupt Service Routine attached to INTx as per attachInterrupt(MP3_DREQINT, refill, RISING)
+ * causes execution of SFEMP3Shield::refill() as per the VS10xx need per DREQ.
+ *
+ * \note MP3_DREQINT corresponds the interrupt vector associated with the pin assigned to MP3_DREQ.
+ *
+ * \note INT(x) may not be available on some base systems depending upon design.
  * Where SFE MP3 Player can use USE_MP3_INTx as DREQ is connected to D2 or INT0.
- * Note that MP3_DREQINT vector is defined above, in pin assignments.
+ * Noting that MP3_DREQINT vector is defined above, in pin assignments.
+ */
+#define USE_MP3_INTx        0 
+
+/**
+ * \brief A macro of the enumerated value used to select Software polling as the means to refill the VS10xx
  *
- * The use of USE_MP3_Timer1 interrupt requires the TimerOne.h library can be 
+ * Where Main loop uses SFEMP3Shield::available() to check if DREQ needs refilling on a periodic basis. 
+ *
+ * \note In this means SFEMP3Shield::available() is simply SFEMP3Shield::refill() 
+ * and MP3_REFILL_PERIOD is \em NOT used with this means.
+ */
+#define USE_MP3_Polled      1 
+
+/**
+ * \brief A macro of the enumerated value used to select Timer1's interrupt as the means to refill the VS10xx
+ *
+ * Where the Interrupt Service Route attached to Timer1 cause periodic execution of SFEMP3Shield::refill()
+ *
+ * \note MP3_REFILL_PERIOD is required when using this means.
+ *
+ * \sa The use of USE_MP3_Timer1 interrupt requires the TimerOne.h library can be 
  * downloaded from http://code.google.com/p/arduino-timerone/ for library.
+ */
+#define USE_MP3_Timer1      2 
+
+/**
+ * \brief A macro of the enumerated value used to select Soft SimpleTimer period as the means to refill the VS10xx
  *
- * The use of USE_MP3_SimpleTimer interrupt requires the TimerOne.h library can
+ * Where Main loop uses SFEMP3Shield::available() to check if DREQ needs refilling on a periodic basis. 
+ *
+ * \note In this means SFEMP3Shield::available() is gating the excecution of SFEMP3Shield::refill() 
+ * based on MP3_REFILL_PERIOD. Where MP3_REFILL_PERIOD is required when using this means.
+ *
+ * \note The associated <SimpleTimer.h> library is required and utilizes 170 more bytes.
+ *
+ * \sa The use of USE_MP3_SimpleTimer interrupt requires the TimerOne.h library can
  * be downloaded from 
  * http://www.arduino.cc/playground/Code/SimpleTimer#GetTheCode for library.
- *
- * Remember to restart Arduino IDE for new Libraries to be available. 
- * Coping the file is not enough.
  */
+#define USE_MP3_SimpleTimer 3 
+
+#endif
 
 //------------------------------------------------------------------------------
-/**
+/*
  * When means other than Polled and INTx are used the following Libraries need to be loaded.
  */
 #if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS == USE_MP3_Timer1
@@ -115,12 +228,16 @@ an UNO/Duemilanove, when left un-altered.
 
 
 //------------------------------------------------------------------------------
-/**
+/*
    When means are time based, need to define the period of update.
    100ms is recommened for 192K sample rate MP3. other rates may vary.
  */
 #if defined(USE_MP3_REFILL_MEANS) && USE_MP3_REFILL_MEANS > USE_MP3_Polled // not needed if INTx is used or polled in loop.
-#define MP3_REFILL_PERIOD 100 // ms for the refill period
+
+/**
+ * \brief A macro used to determine the number of milliseconds between software polls of the DREQ.
+ */
+#define MP3_REFILL_PERIOD 100
 #endif
 
 #endif  // SFEMP3ShieldConfig_h
---
+++ SFEMP3Shield/SFEMP3Shieldmainpage.h
@@ -0,0 +1,245 @@
+/**
+\file SFEMP3mainpage.h
+
+\brief Main Page of MarkDown Documentation
+\remarks implemented with Doxygen Markdown format
+
+\mainpage Arduino %SFEMP3Shield Library
+
+\tableofcontents
+
+<CENTER>Copyright &copy; 2012
+</CENTER>
+
+\section Intro Introduction
+The Arduino %SFEMP3Shield Library is a driver for VSLI's VS10xx, 
+implemented as a Slave co-processor to audio decode streams of Ogg Vorbis/MP3/AAC/WMA/FLAC/WAVMIDI formats,
+across the SPI bus of the Arduino. 
+Principally this library is developed for the VS1053, where it may be compatible with other VS10xx's
+
+Initial development was implemented on an Arduino 328 UNO/Duemilanove with a SparkFun MP3 Player Shield.
+Where additional support has been provided for Seeduino MP3 Player Shield 
+and documention is provied as to how to implement on a Arduino Mega. 
+Where this driver is modular in concept to allow ready porting to other Arduino or Wiring platforms.
+
+The main classes in %SFEMP3Shield is SFEMP3Shield.
+
+\section  Contributers Contributers
+\author  Nathan Seidle, www.sparkfun.com
+\author  Bill Porter, www.billporter.info
+\author  Michael Flaga, www.flaga.net
+\author  ddz
+\author  Wade Brainerd
+
+The SFEMP3Shield use SdFile class 
+
+The SdVolume class supports FAT16 and FAT32 partitions.  Most applications
+will not need to call SdVolume member function.
+
+The Sd2Card class supports access to standard SD cards and SDHC cards.  Most
+applications will not need to call Sd2Card functions.  
+The Sd2Card class can be used for raw access to the SD card.
+
+\todo
+speed up digitalwrites by using SdFat's atomwrite
+
+An example is provided in the %SFEMP3Shield/examples folder.  Which is 
+developed to test %SFEMP3Shield and illustrate its various common uses.
+
+\todo
+%SdFat was developed for high speed data recording.  %SdFat was used to
+implement an audio record/play class, WaveRP, for the Adafruit Wave Shield.
+This application uses special Sd2Card calls to write to contiguous files in
+raw mode.  These functions reduce write latency so that audio can be
+recorded with the small amount of RAM in the Arduino.
+
+\section SDcard SD\SDHC Cards
+
+Arduinos access SD cards using the cards SPI protocol.  PCs, Macs, and
+most consumer devices use the 4-bit parallel SD protocol.  A card that
+functions well on A PC or Mac may not work well on the Arduino.
+
+Most cards have good SPI read performance but cards vary widely in SPI
+write performance.  Write performance is limited by how efficiently the
+card manages internal erase/remapping operations.  The Arduino cannot
+optimize writes to reduce erase operations because of its limit RAM.
+
+SanDisk cards generally have good write performance.  They seem to have
+more internal RAM buffering than other cards and therefore can limit
+the number of flash erase operations that the Arduino forces due to its
+limited RAM.
+
+\section Hardware Hardware Configuration
+
+%SFEMP3Shield was developed using an
+<A HREF = "http://www.sparkfun.com/"> Sparkfun Electonics</A> 
+<A HREF = "https://www.sparkfun.com/products/10628"> MP3 Player Shield DEV-10628</A>.
+
+The hardware interface to the SD card should not use a resistor based level
+shifter.  %SdFat sets the SPI bus frequency to 8 MHz which results in signal
+rise times that are too slow for the edge detectors in many newer SD card
+controllers when resistor voltage dividers are used.
+
+The 5 to 3.3 V level shifter for 5 V Arduinos should be IC based like the
+74HC4050N based circuit shown in the file SdLevel.png.  The Adafruit Wave Shield
+uses a 74AHC125N.  Gravitech sells SD and MicroSD Card Adapters based on the
+74LCX245.
+
+If you are using a resistor based level shifter and are having problems try
+setting the SPI bus frequency to 4 MHz.  This can be done by using 
+card.init(SPI_HALF_SPEED) to initialize the SD card.
+
+\section comment Bugs and Comments
+
+If you wish to report bugs or have comments, send email to fat16lib@sbcglobal.net.
+
+\section SdFatClass SdFat Usage
+
+%SdFat uses a slightly restricted form of short names.
+Only printable ASCII characters are supported. No characters with code point
+values greater than 127 are allowed.  Space is not allowed even though space
+was allowed in the API of early versions of DOS.
+
+Short names are limited to 8 characters followed by an optional period (.)
+and extension of up to 3 characters.  The characters may be any combination
+of letters and digits.  The following special characters are also allowed:
+
+$ % ' - _ @ ~ ` ! ( ) { } ^ # &
+
+Short names are always converted to upper case and their original case
+value is lost.
+
+\note
+  The Arduino Print class uses character
+at a time writes so it was necessary to use a \link SdFile::sync() sync() \endlink
+function to control when data is written to the SD card.
+
+\par
+An application which writes to a file using print(), println() or
+\link SdFile::write write() \endlink must call \link SdFile::sync() sync() \endlink
+at the appropriate time to force data and directory information to be written
+to the SD Card.  Data and directory information are also written to the SD card
+when \link SdFile::close() close() \endlink is called.
+
+\par
+Applications must use care calling \link SdFile::sync() sync() \endlink
+since 2048 bytes of I/O is required to update file and
+directory information.  This includes writing the current data block, reading
+the block that contains the directory entry for update, writing the directory
+block back and reading back the current data block.
+
+It is possible to open a file with two or more instances of SdFile.  A file may
+be corrupted if data is written to the file by more than one instance of SdFile.
+
+
+\section  References References
+\see
+
+<A HREF = "http://www.vlsi.fi/en/products/vs1053.html"> VS1053 Datasheet</A>.
+
+<A HREF = "http://www.vsdsp-forum.com/phpbb/index.php"> VLSI's DSP support Forum</A>.
+
+<A HREF = "http://www.vlsi.fi/en/support/software.html"> VLSI's software download of Apps, Patches, Plugins and tools</A>.
+
+<A HREF = "http://www.vlsi.fi/fileadmin/app_notes/vlsi/vs10XXan_output.pdf"> VS10XX AppNote: Connecting analog outputs</A>.
+
+<A HREF = "http://www.sparkfun.com/"> Sparkfun Electonics</A> 
+
+<A HREF = "https://www.sparkfun.com/products/10628"> MP3 Player Shield DEV-10628</A>.
+
+<A HREF = "http://dlnmh9ip6v2uc.cloudfront.net/datasheets/Dev/Arduino/Shields/MP3%20Shield-v13.pdf"> SFE's Schematic</A>.
+
+<A HREF = "http://www.adafruit.com/"> Adafruit Industries:</A>.
+
+<A HREF = "http://www.arduino.cc/"> The Arduino site</A>.
+
+<A HREF = "http://www.atmel.com/dyn/resources/prod_documents/doc8161.pdf"> The ATmega328 datasheet</A>.
+
+\section  history history
+<pre>
+0.1 made into library, external interrupt driven.
+0.2 included pre-modified SDFat Library
+0.3 added isPlaying function to query shield status
+0.4 added functions to cancel and resume external interrupt
+     in case something else is on the SPI bus	
+0.5 added skipTo() and related functions to skip around in track
+0.6 fixed for Arduino Mega use by calling SDfatlib properly. 
+     Blame Nathan for bad implentation of SDFatlib
+0.7 added functions to read track title,artist,album
+     fixed silly use of static where it shouldn't have been
+0.7.1 chomp'd non ASCII characters from file names.
+0.7.2 Added quick check if trackname is mp3 extension.
+0.7.3 Added apply patch/plugins from SdCard file to VS1xxx.
+0.8 moved MP3 functions into class and cleaned up syntax
+     finished bitrate_table[] table with last row, that was missed.
+     added "d" command to print directory of SdCard
+     added "+/-" command to change volume by 1.0 dB
+     added print of FreeRam() to show amount of static RAM available.
+     save 220 bytes by using F() function to put strings into Flash and not use RAM: 
+       i.e. Serial.Print(F("Hello)");
+     note: FreeRam() is supplied with SdFatUtil.h
+0.9 Added SFEMP3ShieldConfig.h to support alternate hardware for none INT0 DREQ based
+     cards and or Shields. By using Timers, or software pollings, such as with Mega and Seediunos.
+</pre>
+
+\section  Installation Installation
+To install, unzip and place 'SFEMP3Shield', 'SdFat' and 'plugins' folder into your 'C:\Users\{user name}\Documents\Arduino\libraries' folder or '{Arduino IDE path}\hardware\libraries" or {Arduino IDE path}\libraries" directory. 
+Restart the Arduino IDE, and open up the example sketch. 
+
+Most of the uses of the library are in the example sketch. 
+
+\section Error_Codes Error_Codes
+From the 'begin' function:
+<pre>
+0 OK
+1 SD Card Init Failure
+2 SD Card File System (FAT) init failure
+3 SD Card Root Directory init failure
+4 MP3 Decoder mode failure
+5 MP3 Decoder speed failure
+</pre>
+
+From the 'playTrack' or 'playMP3' function:
+<pre>
+0 OK
+1 Already playing track
+2 File not found
+</pre>
+
+From the 'skipTo' function
+<pre>
+0 OK
+1 Not Playing track
+2 Failed to skip to new file location
+</pre>
+
+\section Plug_Ins Plug Ins
+
+.\vs_plg_to_bin.pl is a perl script that will read and digest the .plg files
+and convert them to raw binary as to be read by SFEMP3Shield::VSLoadUserCode() from the SdCard.
+Allowing updates to the VSDsp into its volatile memory after each reset.
+These updates may be custom features or accumilated patches. 
+
+By storing them on the SdCard these plug-ins do not consume the Arduino's limited Flash spaces
+
+Below are pre-compiled binarys of corresponding provided VSLI patches/plugins. 
+The filenames are kept short as SdCard only support 8.3.
+
+<pre>
+.\\pcm.053       .\\vs1053-pcm110\\vs1053pcm.plg                 
+.\\admxleft.053  .\\vs1053b-admix130\\admix-left.plg             
+.\\admxmono.053  .\\vs1053b-admix130\\admix-mono.plg             
+.\\admxrght.053  .\\vs1053b-admix130\\admix-right.plg            
+.\\admxster.053  .\\vs1053b-admix130\\admix-stereo.plg           
+.\\admxswap.053  .\\vs1053b-admix130\\admix-swap.plg             
+.\\patchesf.053  .\\vs1053b-patches195\\vs1053b-patches-flac.plg 
+.\\patches.053   .\\vs1053b-patches195\\vs1053b-patches.plg      
+.\\rtmidi.053    .\\vs1053b-rtmidistart\\rtmidistart.plg         
+.\\eq5.053       .\\vs1053b-eq5-090\\vs1053b-eq5.plg        
+</pre>
+
+These plugins should be placed in the root of the SdCard.
+
+
+
+ */  

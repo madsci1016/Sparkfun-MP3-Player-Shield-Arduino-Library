c7c4319cdac9244f2b3184e91840e817916fd7ba
Michael P. Flaga <michael@flaga.net>
2012-12-16 21:06:24 -0500

    Added ADMixerLoad & ADMixerVol member functions. added commented example in the setup of the INO. along with VS_LINE1_MODE Tab and white space clean up. replaced Mp3WriteRegister of SCI_MODE to preserve uninterested bits, now that Mp3ReadRegister works reliably.

--- search/all_61.js
+++ search/all_61.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['admixerload',['ADMixerLoad',['../class_s_f_e_m_p3_shield.html#a40c1c5874c8edc63c3e2eb4d346fd2e3',1,'SFEMP3Shield']]],
+  ['admixervol',['ADMixerVol',['../class_s_f_e_m_p3_shield.html#a3b1696f32ac61f8d78d538cb5b5ee2db',1,'SFEMP3Shield']]],
   ['available',['available',['../class_s_f_e_m_p3_shield.html#a6a0a8fdb064732ff6e98cc4a717600a5',1,'SFEMP3Shield']]],
   ['arduino_20sfemp3shield_20library',['Arduino SFEMP3Shield Library',['../index.html',1,'']]]
 ];
--- search/functions_61.js
+++ search/functions_61.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['admixerload',['ADMixerLoad',['../class_s_f_e_m_p3_shield.html#a40c1c5874c8edc63c3e2eb4d346fd2e3',1,'SFEMP3Shield']]],
+  ['admixervol',['ADMixerVol',['../class_s_f_e_m_p3_shield.html#a3b1696f32ac61f8d78d538cb5b5ee2db',1,'SFEMP3Shield']]],
   ['available',['available',['../class_s_f_e_m_p3_shield.html#a6a0a8fdb064732ff6e98cc4a717600a5',1,'SFEMP3Shield']]]
 ];

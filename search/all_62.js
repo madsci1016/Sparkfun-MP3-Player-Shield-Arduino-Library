e2809186a95f1e9938389563cb547b9c61d5b9c6
Michael P. Flaga <michael@flaga.net>
2014-02-04 11:40:16 -0500

    added support for BareTouch pinout in config

--- search/all_62.js
+++ search/all_62.js
@@ -3,6 +3,7 @@ var searchData=
   ['b_5fnext',['B_NEXT',['../_m_p3_button_player_8ino.html#a6eae851bf8eb156c0b9132fe2077d802',1,'B_NEXT():&#160;MP3ButtonPlayer.ino'],['../_m_p3_button_player_8ino.html#aeb5ee4ecfd5f871e2d9e9263459a63e8',1,'b_Next():&#160;MP3ButtonPlayer.ino']]],
   ['b_5fplay',['B_PLAY',['../_m_p3_button_player_8ino.html#afab3a5ef07156aa034167ed1e0493e95',1,'B_PLAY():&#160;MP3ButtonPlayer.ino'],['../_m_p3_button_player_8ino.html#a5bcc9e9389bddc95f597df85e58e77ca',1,'b_Play():&#160;MP3ButtonPlayer.ino']]],
   ['b_5fstop',['B_STOP',['../_m_p3_button_player_8ino.html#a139aee074b37c15b632fccf0d5a5c71a',1,'B_STOP():&#160;MP3ButtonPlayer.ino'],['../_m_p3_button_player_8ino.html#aec6621818fa8a6422f04915dfe6d99d5',1,'b_Stop():&#160;MP3ButtonPlayer.ino']]],
+  ['baretouch',['BARETOUCH',['../_s_f_e_m_p3_shield_config_8h.html#a5649127c062be5324d8ed4a3b7fa2617',1,'SFEMP3ShieldConfig.h']]],
   ['begin',['begin',['../class_s_f_e_m_p3_shield.html#a3216f2746910835a1e46e7f4233889f9',1,'SFEMP3Shield']]],
   ['bitrate',['bitrate',['../class_s_f_e_m_p3_shield.html#a9756e8739e4757834117f25ca589b440',1,'SFEMP3Shield']]],
   ['bitrate_5ftable',['bitrate_table',['../_s_f_e_m_p3_shield_8cpp.html#a00f070121843c83052db04adf1ab1ee5',1,'SFEMP3Shield.cpp']]],

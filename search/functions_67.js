6e902cf78c444ff7d65fc60bbec0a10755e33ec6
Michael P. Flaga <michael@flaga.net>
2014-02-08 23:41:25 -0500

    removed unused test code and new doc files

--- search/functions_67.js
+++ search/functions_67.js
@@ -8,7 +8,6 @@ var searchData=
   ['getearspeaker',['getEarSpeaker',['../class_s_f_e_m_p3_shield.html#a9f24638f19da7d1cd393b2fb958bdc70',1,'SFEMP3Shield']]],
   ['getmonomode',['getMonoMode',['../class_s_f_e_m_p3_shield.html#aa76395229f67f4560e561d5ba20d9d30',1,'SFEMP3Shield']]],
   ['getplayspeed',['getPlaySpeed',['../class_s_f_e_m_p3_shield.html#a16250d6bed9c59271dc83b0ee8a3cc34',1,'SFEMP3Shield']]],
-  ['getsci_5fbase',['getSCI_BASE',['../_file_player_8ino.html#a32cf07590a2d0bde9903262d32da3878',1,'FilePlayer.ino']]],
   ['getstate',['getState',['../class_s_f_e_m_p3_shield.html#a28e9dbe66184c97ba8345aabaa8cc057',1,'SFEMP3Shield']]],
   ['gettrackinfo',['getTrackInfo',['../class_s_f_e_m_p3_shield.html#a6a4b776e45e151a7c330d91a9e88fb78',1,'SFEMP3Shield']]],
   ['gettrebleamplitude',['getTrebleAmplitude',['../class_s_f_e_m_p3_shield.html#aba73702151e2a7e3eebd94710bd69a35',1,'SFEMP3Shield']]],
---
+++ search/variables_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nibble',['nibble',['../union_s_f_e_m_p3_shield_1_1sci__bass__m.html#ae1841d1345297deafea6728677389494',1,'SFEMP3Shield::sci_bass_m']]]
+];
